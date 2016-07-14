f27493b05fe900b6267a54eb9d5111ea065e85e7
tpanum <thomas@panum.dk>
2016-07-14 15:16:31 +0200

    Fixed issues

--- scripts/backdoor_cron.sh
+++ scripts/backdoor_cron.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-COMMAND="ssh -R server.panum.dk:{{@PORT}}:localhost:22 server.panum.dk"
+COMMAND="ssh -oStrictHostKeyChecking=no -R server.panum.dk:{{@PORT}}:localhost:22 server.panum.dk"
 pgrep -f -x "$COMMAND" > /dev/null 2>&1 || $COMMAND
